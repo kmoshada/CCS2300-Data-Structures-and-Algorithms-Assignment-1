f5d745dc29056609763baa15f33cb51eef958aca
Lakrandi <lakrandikobewaththa@gmail.com>
2026-02-04 21:50:16 +0530

    update the DataGenerator , performanze analyzer , serchingAlogorithms and sortingAlgorithms files

---
+++ Assignment/src/Model03/DataGenerator.java
@@ -0,0 +1,17 @@
+import java.util.Random;
+
+//This class generates random arrays
+public class DataGenerator{
+  public static int [] generateArray(int size){
+
+      Random random = new Random();
+      int[] arr = new int[size];
+
+      for (int i = 0 ; i <size; i++){
+          arr[i] = random.nextInt(10000);
+      }
+      return arr;
+      
+  }
+
+}
---
+++ Assignment/src/Model03/SortingAlgorithms.java
@@ -0,0 +1,44 @@
+//This class contains Quick sort 
+
+public class SortingAlgorithms{
+
+//partition method(pivot=first element) 
+
+public static int partition(int[] A, int lb, int ub){
+    int pivot = A[lb]; //Get first element as pivot
+    int start = lb;
+    int end =ub;
+
+    while (start < end ){
+        while(A[start] <= pivot && start < ub){
+            start++;
+        }
+        while (A[end] > pivot){
+            end--;
+        }
+        if(start < end) {
+          int temp = A[start];
+          A [start]=A [end];
+          A [end] = temp;
+
+        }
+    }
+    //place pivot in correct position
+         int temp = A[lb];
+         A[lb] = A[end];
+         A[end] = temp;
+
+          return end;
+
+}
+// USE Quick sort method
+public static void quicksort(int[] A,int lb,int ub){
+    if(lb < ub){
+       int k= partition(A,lb,ub);
+
+            quicksort(A,lb,k-1);//left side
+            quicksort(A,k+1,ub);//right side
+
+      }
+ }
+}
